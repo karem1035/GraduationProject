9d5dffe540347955b0fc4ab6fa706d44778dd668
Asmaa Gadallah <asmaagadallaah@gmail.com>
2026-02-24 07:53:00 +0200

    Ignored target files

--- Toda/src/main/java/com/example/Toda/service/authService.java
+++ Toda/src/main/java/com/example/Toda/service/authService.java
@@ -4,8 +4,6 @@ import com.example.Toda.DTO.RegisterRequest;
 import com.example.Toda.DTO.authResponse;
 import com.example.Toda.Entity.*;
 import com.example.Toda.exception.UserAlreadyExistsException;
-import com.example.Toda.repo.TourGuideRepo;
-import com.example.Toda.repo.TouristProfileRepo;
 import com.example.Toda.repo.UserRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,18 +18,12 @@ public class authService {
     private final UserRepo userRepo;
     private final PasswordEncoder passwordEncoder;
     private final JWTService jwtService;
-    private final TourGuideRepo tourGuideRepo;
-    private final TouristProfileRepo touristProfileRepo;
-    //@Autowired
-  //  private OtpProducer otpProducer;
     Map<String, Object> claims = new HashMap<>();
 
-    public authService(UserRepo userRepo, PasswordEncoder passwordEncoder, JWTService jwtService, TourGuideRepo tourGuideRepo, TouristProfileRepo touristProfileRepo) {
+    public authService(UserRepo userRepo, PasswordEncoder passwordEncoder, JWTService jwtService) {
         this.userRepo = userRepo;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
-        this.tourGuideRepo = tourGuideRepo;
-        this.touristProfileRepo = touristProfileRepo;
     }
     @Transactional
 
@@ -47,19 +39,6 @@ public class authService {
         Role role = Role.valueOf(registerRequest.role().toUpperCase());
         newUser.setRole(role);
         UserEntity savedUser =userRepo.save(newUser);
-        if(savedUser.getRole().equals(Role.TOURGUIDE)){
-            TourGuideEntity tourGuide = new TourGuideEntity();
-            tourGuide.setUser(savedUser);
-            tourGuide.setName(savedUser.getUsername());
-            tourGuide.setEmail(savedUser.getEmail());
-            tourGuideRepo.save(tourGuide);
-        }else {
-            TouristProfileEntity touristProfile = new TouristProfileEntity();
-            touristProfile.setUser(savedUser);
-            touristProfile.setName(savedUser.getUsername());
-            touristProfile.setEmail(savedUser.getEmail());
-            touristProfileRepo.save(touristProfile);
-        }
         UserPrincipal userPrincipal = new UserPrincipal(newUser);
         String token = jwtService.generateToken(claims, userPrincipal);
 
